c88fc7ccfe2e510088e9f84f9173e806f66d9c9d
stmamo <stmamo@mum.edu>
2019-09-22 19:48:01 -0500

    ide setup

--- src/main/java/model/Post.java
+++ src/main/java/model/Post.java
@@ -1,14 +1,17 @@
 package model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Blob;
 
 @Entity
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    long id;
+    private long id;
+    private String text;
+    @OneToMany(mappedBy="post")
+    private Picture picture;
+    private boolean enabled;
+
 }
