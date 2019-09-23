f3cf3997a2638e2dcb228c51d5260edb6f6d1c99
stmamo <stmamo@mum.edu>
2019-09-22 19:39:32 -0500

    ide setup

---
+++ src/main/java/model/Post.java
@@ -0,0 +1,14 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+}
